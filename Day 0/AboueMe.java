7f2189916652ea92b2a9ef5a1ce675d5ada77d67
l-hsu <liamhsu@icloud.com>
2026-08-28 14:42:20 -0700

    Day 1

---
+++ Day 0/AboueMe.java
@@ -0,0 +1,5 @@
+public class AboueMe {
+    public static void main(String[] args) {
+        System.out.println("Name: Liam Hsu\nBirthday: June 24, 2010\nHobbies: Cooking, Video Games, Football\nFavorite Book: Ender's Game\nFavorite Movie: Pacific Rim");
+    }
+}
---
+++ Day 0/BlockLetters.java
@@ -0,0 +1,5 @@
+public class BlockLetters {
+    public static void main(String[] args) {
+        System.out.println("L\nL\nL\nL\nLLLLL\n\nH   H\nH   H\nHHHHH\nH   H\nH   H");
+    }
+}
